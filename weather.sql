3d0226638449cdb773289727231bca25be73f5f6
Gus Mueller <gus@asecular.com>
2022-04-09 11:47:48 -0400

    data should be nullable

--- weather.sql
+++ weather.sql
@@ -1,15 +1,12 @@
-
-
-
 CREATE TABLE weather_data(
 weather_data_id INT AUTO_INCREMENT PRIMARY KEY,
-location_id INT,
+location_id INT NULL,
 recorded DATETIME,
-temperature DECIMAL(6,3),
-pressure DECIMAL(9,4),
-humidity DECIMAL(6,3),
-wind_direction INT,
-precipitation INT,
-wind_speed DECIMAL(8,3),
-wind_increment INT
+temperature DECIMAL(6,3) NULL,
+pressure DECIMAL(9,4) NULL,
+humidity DECIMAL(6,3) NULL,
+wind_direction INT NULL,
+precipitation INT NULL,
+wind_speed DECIMAL(8,3) NULL,
+wind_increment INT NULL
 )
