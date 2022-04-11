0439a9fb7ad01eaf90029cb341e6e00b17008caa
Gus Mueller <gus@asecular.com>
2022-04-11 19:48:56 -0400

    added scale dropdown to show graphs across different amounts of time

--- index.php
+++ index.php
@@ -1,4 +1,36 @@
 <!doctype html>
+<?php 
+
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+
+function genericSelect($id, $name, $defaultValue, $data, $event = "", $handler= "") {
+	$out = "";
+	$out .= "<select name='" . $name. "' id='" . $id . "' " . $event . "=\"" . $handler . "\">\n";
+	foreach($data as $datum) {
+		//echo $datum;
+		$value = $datum->value;
+		$text = $datum->text;
+		$selected = "";
+		if($defaultValue == $value) {
+			$selected = " selected='true'";
+		}
+		$out.= "<option " . $selected . " value=\"" . $value . "\">";
+		$out.= $text;
+		$out.= "</option>";
+	}
+	$out.= "</select>";
+	return $out;
+}
+if(array_key_exists($REQUEST, "locationId")) {
+	$locationId = $_REQUEST["locationId"];
+} else {
+	$locationId = 1;
+
+}
+
+?>
 <html>
 
 <head>
@@ -39,12 +71,23 @@
         <canvas id="Chart" width="400" height="700"></canvas>
     </div>
 <div>
-Location: 
-<select id='locationDropdown' onmouseup="console.log('x');getData(document.getElementById('locationDropdown')[document.getElementById('locationDropdown').selectedIndex].value)">
-<option value='1' >Cabin Upstairs</option>
-<option value='2' >Cabin Downstairs</option>
+<table id="dataTable">
+<?php 
+//lol, it's easier to specify an object in json and decode it that to specify it in PHP
+$selectData = json_decode('[{"text":"Cabin Upstairs","value":1},{"text":"Cabin Downstairs","value":2}]');
+//var_dump($selectData);
+//echo  json_last_error_msg();
+$selectId = "locationDropdown";
+$handler = "getData(document.getElementById('" . $selectId . "')[document.getElementById('" . $selectId  . "').selectedIndex].value)";
 
-</select>
+echo "<tr><td>Location:</td><td>" . genericSelect($selectId, "locationId", $locationId, $selectData, "onchange", $handler) . "</td></tr>";
+
+$handler = "getData(document.getElementById('" . $selectId . "')[document.getElementById('" . $selectId  . "').selectedIndex].value)";
+
+$scaleData = json_decode('[{"text":"detailed","value":"fine"},{"text":"hourly","value":"hour"}, {"text":"daily","value":"day"}]');
+echo "<tr><td>Scale:</td><td>" . genericSelect("scaleDropdown", "scale", "fine", $scaleData, "onchange", $handler) . "</td></tr>";
+?>
+</table>
 <!--
 	<table id="dataTable">
 		<thead>
@@ -69,11 +112,9 @@ let humidityValues = [];
 let pressureValues = [];
 let timeStamp = [];
 
-function showGraph(locationId)
-{
-
-    var ctx = document.getElementById("Chart").getContext('2d');
-    var Chart2 = new Chart(ctx, {
+function showGraph(locationId){
+    let ctx = document.getElementById("Chart").getContext('2d');
+    let Chart2 = new Chart(ctx, {
         type: 'line',
         data: {
             labels: timeStamp,  //Bottom Labeling
@@ -152,55 +193,57 @@ getData("<?php echo $_REQUEST["locationId"]?>");
   //; //50000mSeconds update rate
  
 function getData(locationId) {
-  let xhttp = new XMLHttpRequest();
-  let endpointUrl = "http://randomsprocket.com/weather/data.php?mode=getData&locationId=" + locationId;
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-     //Push the data in array
-		temperatureValues = [];
-		humidityValues = [];
-		pressureValues = [];
-		timeStamp = [];
-		let time = new Date().toLocaleTimeString();
-		let dataObject = JSON.parse(this.responseText); 
-		//let tbody = document.getElementById("tableBody");
-		//tbody.innerHTML = '';
-		
-		for(let datum of dataObject) {
-			console.log(datum);
-			let time = datum[2];
-			let temperature = datum[3];
-			temperature = temperature * (9/5) + 32;
-			//convert temperature to fahrenheitformula
-			let pressure = datum[4];
-			let humidity = datum[5];
-			temperatureValues.push(temperature);
-			humidityValues.push(humidity);
-			pressureSkewed = pressure;//so we can see some detail in pressure
-			if(pressure > 0) {
-				pressureValues.push(pressure); 
-			}
-			timeStamp.push(time);
+	let scale = document.getElementById('scaleDropdown')[document.getElementById('scaleDropdown').selectedIndex].value;
+	let xhttp = new XMLHttpRequest();
+	let endpointUrl = "http://randomsprocket.com/weather/data.php?scale=" + scale + "&mode=getData&locationId=" + locationId;
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	     //Push the data in array
+			temperatureValues = [];
+			humidityValues = [];
+			pressureValues = [];
+			timeStamp = [];
+			let time = new Date().toLocaleTimeString();
+			let dataObject = JSON.parse(this.responseText); 
+			//let tbody = document.getElementById("tableBody");
+			//tbody.innerHTML = '';
 			
-			/*
-			let row = tbody.insertRow(0); //Add after headings
-			let cell1 = row.insertCell(0);
-			let cell2 = row.insertCell(1);
-			let cell3 = row.insertCell(2);
-			cell1.innerHTML = time;
-			cell2.innerHTML = temperature;
-			cell3.innerHTML = humidity;
-			*/
-		}
- 
-		showGraph(locationId);  //Update Graphs
-
-    }
-  };
+			for(let datum of dataObject) {
+				//console.log(datum);
+				let time = datum[2];
+				let temperature = datum[3];
+				temperature = temperature * (9/5) + 32;
+				//convert temperature to fahrenheitformula
+				let pressure = datum[4];
+				let humidity = datum[5];
+				temperatureValues.push(temperature);
+				humidityValues.push(humidity);
+				pressureSkewed = pressure;//so we can see some detail in pressure
+				if(pressure > 0) {
+					pressureValues.push(pressure); 
+				}
+				timeStamp.push(time);
+				
+				/*
+				let row = tbody.insertRow(0); //Add after headings
+				let cell1 = row.insertCell(0);
+				let cell2 = row.insertCell(1);
+				let cell3 = row.insertCell(2);
+				cell1.innerHTML = time;
+				cell2.innerHTML = temperature;
+				cell3.innerHTML = humidity;
+				*/
+			}
+	 
+			showGraph(locationId);  //Update Graphs
+	
+	    }
+	  };
   xhttp.open("GET", endpointUrl, true); //Handle getData server on ESP8266
   xhttp.send();
 }
-    
+
+getData(<?php echo $locationId?>);
 </script>
 </body>
 
