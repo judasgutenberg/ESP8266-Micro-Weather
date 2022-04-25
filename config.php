d0a8fa0fa7fe62ea66deb130847ad890bcd619cc
Gus Mueller <gus@asecular.com>
2022-04-25 13:35:07 -0400

    Update config.php

--- config.php
+++ config.php
@@ -4,4 +4,4 @@ $servername = "localhost";
 $username = "your_sql_username";
 $database = "your_sql_database";
 $password = "your_sql_password";
-$storagePassword = "your_storage_password"; //should match value in arduino sketch
+$storagePassword = "your_storage_password"; //should match value in config.h
